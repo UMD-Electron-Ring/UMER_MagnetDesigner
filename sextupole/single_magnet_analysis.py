2f0d050ce49059b02c1285f7545871b7d7930dab
Noah Gleason <noah.gleason1@gmail.com>
2018-08-15 10:46:05 -0400

    Add support for multipole strength_along_z

--- sextupole/single_magnet_analysis.py
+++ sextupole/single_magnet_analysis.py
@@ -7,7 +7,7 @@ from matplotlib import animation
 import os
 
 
-def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z):
+def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z, n_poles):
     """
     Measure how the sextupole strength of a magnet varies with respect to z.
     :param in_file: The name .spc file representing the magnet. Should not include the path.
@@ -17,8 +17,11 @@ def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z):
     :param radius: The radius to measure strength within. Should be less than the actual radius to avoid weirdness.
     :param along_x: Whether to measure strength along x or along y.
     :param edge_z: The distance from the edge of the magnet to the center along z, in meters.
+    :param n_poles: The number of poles of the magnet strength you want to measure
     :return: a dictionary mapping z-coordinate to sextupole strength.
     """
+    # Calculate polynomial degree from number of poles.
+    degree = int(n_poles / 2. - 1.)
     # We want 1000 steps total, across the diameter.
     r_step = radius * 2. / 1000.
     z_to_strength = {}
@@ -41,6 +44,7 @@ def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z):
         process = subprocess.Popen("./mag -bat batch.bat".split(), stdout=subprocess.PIPE, cwd=folder)
     else:
         process = subprocess.Popen("MagLi.exe -bat batch.bat".split(), stdout=subprocess.PIPE, cwd=folder)
+    out, error = process.communicate()
     for z in np.linspace(start_z, end_z, steps):
         # Round to avoid floating point errors
         z = round(z, 6)
@@ -58,27 +62,15 @@ def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z):
             By.append(line[4])
         r = np.array(r)
         By = np.array(By)
-        poly_fit = poly.polyfit(r, By, 2, full=True)
+        poly_fit = poly.polyfit(r, By, degree, full=True)
         poly_function = poly.polyval(r, poly_fit[0])
         # Calculate R^2
         z_to_r_squared[z] = 1. - (poly_fit[1][0][0] / sum(poly_function ** 2))
-        z_to_strength[z] = poly_fit[0][2] * 2.0
+        # Calculate strength, assuming a 10 KeV electron.
+        z_to_strength[z] = poly.polyder(poly_fit[0], degree)[0] / 0.00033887
+
     # Plot the strength along z.
-    plt.plot(z_to_strength.keys(), z_to_strength.values())
-    # Plot the edges of the sextupole magnet
-    plt.vlines(edge_z, min(z_to_strength.values()), max(z_to_strength.values()), colors="tab:orange",
-               linestyles="dashed")
-    plt.vlines(-edge_z, min(z_to_strength.values()), max(z_to_strength.values()), colors="tab:orange",
-               linestyles="dashed")
-    # Plot y = 0
-    plt.hlines(0, start_z, end_z, linestyles="dashed")
-    plt.xlabel("Z (meters)")
-    plt.ylabel("Sextupole Strength ($\\frac{T}{m^2}$)")
-    if along_x:
-        plt.title("Strength of " + in_file + " along x with radius " + str(radius))
-    else:
-        plt.title("Strength of " + in_file + " along y with radius " + str(radius))
-    plt.show()
+    plot_strength(z_to_strength, degree, start_z, end_z, edge_z, radius, along_x)
 
     # Plot the R^2 along z
     plt.plot(z_to_r_squared.keys(), z_to_r_squared.values())
@@ -94,15 +86,44 @@ def strength_along_z(in_file, start_z, end_z, steps, radius, along_x, edge_z):
     else:
         plt.title("$R^2$ of " + in_file + " along y with radius " + str(radius))
     plt.show()
+
     # Print useful numbers
     print("Integral: " + str(
-        (sum(z_to_strength.values()) * (end_z - start_z) / len(z_to_strength.values()))) + " Tesla per meter")
-    print("Min: " + str(min(z_to_strength.values())) + " Tesla per square meter")
-    print("Max: " + str(max(z_to_strength.values())) + " Tesla per square meter")
-    # This assumes a 10 KeV electron.
-    print("Min: " + str(min(z_to_strength.values()) / 0.00033887) + " inverse cubic meters")
-    print("Max: " + str(max(z_to_strength.values()) / 0.00033887) + " inverse cubic meters")
+        (sum(z_to_strength.values()) * (end_z - start_z) / len(z_to_strength.values()))) + " Tesla * meters^" + str(
+        -degree + 1))
+    print("Min: " + str(min(z_to_strength.values()) * 0.00033887) + " Tesla * meters^" + str(-degree))
+    print("Max: " + str(max(z_to_strength.values()) * 0.00033887) + " Tesla * meters^" + str(-degree))
+    print("Min: " + str(min(z_to_strength.values())) + " meters^" + str(-degree - 1))
+    print("Max: " + str(max(z_to_strength.values())) + " meters^" + str(-degree - 1))
     return z_to_strength
+
+
+def plot_strength(z_to_strength, degree, start_z, end_z, edge_z, radius, along_x):
+    prefix = ""
+    if degree == 0:
+        prefix = "di"
+    elif degree == 1:
+        prefix = "quadru"
+    elif degree == 2:
+        prefix = "sextu"
+    elif degree == 3:
+        prefix = "octu"
+    else:
+        prefix = str(degree * 2 + 2) + "-"
+    plt.plot(z_to_strength.keys(), z_to_strength.values())
+    plt.vlines(edge_z, min(z_to_strength.values()), max(z_to_strength.values()), colors="tab:orange",
+               linestyles="dashed")
+    plt.vlines(-edge_z, min(z_to_strength.values()), max(z_to_strength.values()), colors="tab:orange",
+               linestyles="dashed")
+    # Plot y = 0
+    plt.hlines(0, start_z, end_z, linestyles="dashed")
+    plt.xlabel("Z (meters)")
+    plt.ylabel(prefix.capitalize() + "pole Strength ($m^{" + str(-degree - 1) + "}$)")
+    if along_x:
+        plt.title(prefix.capitalize() + "pole Strength Along x With Radius " + str(radius))
+    else:
+        plt.title(prefix.capitalize() + "pole Strength Along y With Radius " + str(radius))
+    plt.show()
 
 
 def field_plot(in_file, z, radius, grid_square_length, force=False, draw=True):
@@ -217,7 +238,3 @@ def field_animation(in_file, start_z, end_z, steps, radius, grid_square_length, 
     else:
         anim.save("B-field of " + in_file + " from Z=" + str(start_z) + " to Z=" + str(end_z) + ".mp4")
     return anim
-
-
-# strength_along_z("sextupole_routed.spc", -0.045, 0.045, 20, 0.015, True, 0.0245)
-field_plot("sextupole_routed.spc", 0, 0.03, 0.001)
